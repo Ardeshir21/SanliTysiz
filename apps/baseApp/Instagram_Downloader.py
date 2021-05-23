94a8305d532acdc17666da51956727e7c565a164
Ardeshir <examples21@gmail.com>
2021-05-24 00:16:48 +0300

    e

--- apps/baseApp/Instagram_Downloader.py
+++ apps/baseApp/Instagram_Downloader.py
@@ -42,11 +42,11 @@ class crawler():
         vid_urls = self.prepare_urls(vid_matches)
         pic_urls = self.prepare_urls(pic_matches)
 
-        if vid_urls:
-            print('Detected Videos:\n{0}'.format('\n'.join(vid_urls)))
-
-        if pic_urls:
-            print('Detected Pictures:\n{0}'.format('\n'.join(pic_urls)))
+        # if vid_urls:
+        #     print('Detected Videos:\n{0}'.format('\n'.join(vid_urls)))
+        #
+        # if pic_urls:
+        #     print('Detected Pictures:\n{0}'.format('\n'.join(pic_urls)))
 
         if not (vid_urls or pic_urls):
             print('Could not recognize the media in the provided URL.')
@@ -60,29 +60,29 @@ class crawler():
 # new_crawler.list_media_addresses()
 
 
-# if no crawler object exist, make one instance
-requested_url = 'https://www.instagram.com/p/COixLOLjUbF/'
-new_crawler = crawler(requested_url)
-media_addresses = new_crawler.list_media_addresses()
-videos_list = []
-images_list = []
-
-# Download all the files into the Server
-url_path = urlparse(requested_url).path
-url_path = url_path.replace('/', '_')
-
-
-# Vidoes
-for index, file in enumerate(media_addresses['videos_addresses'], start=1):
-    file_name = '{}_{}.mp4'.format(url_path, index)
-    temp_file_path = os.path.join('/home/ardeshir/SanliTysiz/media/', 'Downloads', file_name)
-    urlretrieve(file)
-    # add file name to the list
-    videos_list.append(file_name)
-# Images
-for index, file in enumerate(media_addresses['images_addresses'], start=1):
-    file_name = '{}_{}.jpg'.format(url_path, index)
-    temp_file_path = os.path.join('/home/ardeshir/SanliTysiz/media/', 'Downloads', file_name)
-    urlretrieve(file, temp_file_path)
-    # add file name to the list
-    images_list.append(file_name)
+# # if no crawler object exist, make one instance
+# requested_url = 'https://www.instagram.com/p/COixLOLjUbF/'
+# new_crawler = crawler(requested_url)
+# media_addresses = new_crawler.list_media_addresses()
+# videos_list = []
+# images_list = []
+#
+# # Download all the files into the Server
+# url_path = urlparse(requested_url).path
+# url_path = url_path.replace('/', '_')
+#
+#
+# # Vidoes
+# for index, file in enumerate(media_addresses['videos_addresses'], start=1):
+#     file_name = '{}_{}.mp4'.format(url_path, index)
+#     temp_file_path = os.path.join('/home/ardeshir/SanliTysiz/media/', 'Downloads', file_name)
+#     urlretrieve(file)
+#     # add file name to the list
+#     videos_list.append(file_name)
+# # Images
+# for index, file in enumerate(media_addresses['images_addresses'], start=1):
+#     file_name = '{}_{}.jpg'.format(url_path, index)
+#     temp_file_path = os.path.join('/home/ardeshir/SanliTysiz/media/', 'Downloads', file_name)
+#     urlretrieve(file, temp_file_path)
+#     # add file name to the list
+#     images_list.append(file_name)
